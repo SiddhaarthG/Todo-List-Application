8e40f8b41954b350b55e03ffaa9e63f1bc50897c
Siddhaarth <gangarajsiddhaarth@gmail.com>
2023-03-24 12:19:38 +0530

    Added lint-staged

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,36 +1,43 @@
+/* eslint-disable no-undef */
 const todoList = () => {
-  all = []
+  all = [];
   const add = (todoItem) => {
-    all.push(todoItem)
-  }
+    all.push(todoItem);
+  };
   const markAsComplete = (index) => {
-    all[index].completed = true
-  }
+    all[index].completed = true;
+  };
 
   const overdue = () => {
     // Write the date check condition here and return the array
     // of overdue items accordingly.
-    return (all.filter((todo) => todo.dueDate === yesterday));
-  }
+    return all.filter((todo) => todo.dueDate === yesterday);
+  };
 
   const dueToday = () => {
     // Write the date check condition here and return the array
     // of todo items that are due today accordingly.
-    return (all.filter((todo) => todo.dueDate === today));
-  }
+    return all.filter((todo) => todo.dueDate === today);
+  };
 
   const dueLater = () => {
     // Write the date check condition here and return the array
     // of todo items that are due later accordingly.
-    return (all.filter((todo) => todo.dueDate === tomorrow));
-  }
+    return all.filter((todo) => todo.dueDate === tomorrow);
+  };
 
   const toDisplayableList = (list) => {
     // Format the To-Do list here, and return the output string
     // as per the format given above.
-    return (list.map((todo) => 
-        `${todo.completed === true ? '[x]' : '[ ]'} ${todo.title} ${todo.dueDate === today ? ' ': todo.dueDate}`).join('\n'));
-  }
+    return list
+      .map(
+        (todo) =>
+          `${todo.completed === true ? "[x]" : "[ ]"} ${todo.title} ${
+            todo.dueDate === today ? " " : todo.dueDate
+          }`
+      )
+      .join("\n");
+  };
 
   return {
     all,
@@ -39,7 +46,7 @@ const todoList = () => {
     overdue,
     dueToday,
     dueLater,
-    toDisplayableList
+    toDisplayableList,
   };
 };
 
