77f30b702336c815bd17b77c0ce91a183db8aa3d
Siddhaarth <gangarajsiddhaarth@gmail.com>
2023-03-24 12:41:37 +0530

    check retrieval

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,28 +1,67 @@
-const todoList = require('../todo')
+const todoList = require("../todo");
 
-const {all, markAsComplete, add} = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
-describe("First Test Suite",()=>{
-    beforeAll(() => {
-        add({
-            title:"Todo test",
-            complete: false,
-            dueDate: new Date().toISOString().slice(0,10)
-        });
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
+
+var dateToday = new Date();
+const today = formattedDate(dateToday);
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+);
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+);
+
+describe("Todo Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Todo test 1",
+      complete: false,
+      dueDate: today,
     });
+    add({
+      title: "Todo test 2",
+      complete: false,
+      dueDate: tomorrow,
+    });
+    add({
+      title: "Todo test 3",
+      complete: false,
+      dueDate: yesterday,
+    });
+  });
 
-    test("Should add new item",()=>{
-        const todoListCount = all.length
-        add({
-            title:"Todo test",
-            complete: false,
-            dueDate: new Date().toISOString().slice(0,10)
-        })
-        expect(all.length).toBe(todoListCount+1)
-    })
+  test("Should add new item", () => {
+    const todoListCount = all.length;
+    add({
+      title: "Todo test 4",
+      complete: false,
+      dueDate: today,
+    });
+    expect(all.length).toBe(todoListCount + 1);
+  });
 
-    test("Should mark as complete",() => {
-        markAsComplete(0)
-        expect(all[0].completed).toBe(true)
-    })
-})
+  test("Should mark as complete", () => {
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+
+  test("Check retrieval of today due items", () => {
+    let todayDue = dueToday();
+    expect(todayDue[0]).toBe(all[0]);
+    expect(todayDue[1]).toBe(all[3]);
+  });
+
+  test("Check retrieval of tomorrow due items", () => {
+    let tomorrowDue = dueLater();
+    expect(tomorrowDue[0]).toBe(all[1]);
+  });
+
+  test("Check retrieval of yesterday due items", () => {
+    let yesterdayDue = overdue();
+    expect(yesterdayDue[0]).toBe(all[2]);
+  });
+});
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,4 +1,17 @@
 /* eslint-disable no-undef */
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
+
+var dateToday = new Date();
+const today = formattedDate(dateToday);
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+);
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+);
+
 const todoList = () => {
   all = [];
   const add = (todoItem) => {
