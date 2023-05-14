51319b1225afaaa67aca33de917675b6de6002be
Siddhaarth <gangarajsiddhaarth@gmail.com>
2023-05-14 18:36:43 +0530

    define association

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -12,6 +12,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      Todo.belongsTo(models.User, {
+        foreignKey: "userId",
+      });
       // define association here
     }
 
